9c68fb6f8fa049fb7094433cb1a2b3910417dc2a
David Paarup <david@letsreg.no>
2025-07-23 11:10:20 +0200

    Added an image to community highlights

--- schemaTypes/communityHighlight.ts
+++ schemaTypes/communityHighlight.ts
@@ -9,7 +9,11 @@ export const communityHighlight = defineType({
     { name: 'artwork', type: 'string', title: 'Artwork' },
     { name: 'medium', type: 'string', title: 'Medium' },
     { name: 'likes', type: 'number', title: 'Likes' },
-    // Optionally add an image field:
-    // { name: 'image', type: 'image', title: 'Artwork Image' },
+    { 
+      name: 'image', 
+      type: 'image', 
+      title: 'Artwork Image',
+      options: { hotspot: true },
+    },
   ],
 })
